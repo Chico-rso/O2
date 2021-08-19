ffd126051471f3c3a48dac585f02c567e36b4d57
O2 <o2@Mac-mini-O2.local>
2021-08-19 16:22:33 +0300

    correct catalog-item and add new slider

--- src/blocks/promotions/promotions.js
+++ src/blocks/promotions/promotions.js
@@ -1,46 +1,67 @@
-// o2.promotions =
-// {
-// 	slideIndex: 1,
-// 	slides: [],
-// 	dots: [],
-// 	dot: 1,
-// 	showSlides(n)
-// 	{
-// 		if(n > this.slides.length)
-// 		{
-// 			n = 1;
-// 		}
+o2.promotions =
+{
+	slideIndex:0,
+	slides: [],
+	dots: [],
+	initSlider()
+	{
+		this.slides = document.querySelectorAll('.promo__slider-item');
+		this.sliderWrapper = document.querySelector('.promo__slider-wraper');
+		this.slidesField = document.querySelector('.promo__slider-list');
+		this.width = this.sliderWrapper.offsetWidth;
+		this.dots = document.querySelectorAll('._dot');
+		this.addListener();
+	},
+	goToSlide(dir)
+	{
+		if(dir === "next")
+		{
+			if (this.slideIndex + 1 === this.slides.length)
+			{
+				this.slideIndex = 0;
+			}
+			else
+			{
+				this.slideIndex++;
+			}
+		}
+		else
+		{
+			if (this.slideIndex -1 < 0)
+			{
+				this.slideIndex = this.slides.length - 1;
+			}
+			else
+			{
+				this.slideIndex--;
+			}
+		}
+		this.position(this.slideIndex);
 
-// 		if(n < 1)
-// 		{
-// 			n = this.slides.length;
-// 		}
-
-// 		this.slides.forEach(item => item.style.display = 'none' );
-
-// 		this.slides[n - 1].style.display = 'flex';
-
-// 		this.slideIndex = n;
-
-// 		this.useDots(n);
-// 	},
-// 	plusSlides(n)
-// 	{
-// 		this.showSlides(this.slideIndex + n);
-// 		console.log(this.slides);
-// 		console.log(this.slideIndex);
-// 	},
-// 	useDots(n)
-// 	{
-// 		let slider = document.querySelector('._promo__slider'),
-// 		dots = slider.querySelectorAll('._dot');
-// 		dots.forEach(el => el.classList.add('opacity'));
-// 		console.log(dots[n]);
-// 		dots[n - 1].classList.remove('opacity');
-// 	},
-// 	initSlider()
-// 	{
-// 		this.slides = document.querySelectorAll('.slider-item');
-// 		this.showSlides(1);
-// 	}
-// }
+	},
+	position(index)
+	{
+		let transform = 0;
+		transform = (index * this.width) * -1;
+		this.slidesField.style.transform = `translateX(${transform}px)`;
+		this.dots.forEach((el, i) =>
+		{
+			if(i === index)
+			{
+				el.classList.add('active');
+			}
+			else
+			{
+				el.classList.remove('active');
+			}
+		})
+	},
+	addListener()
+	{
+		window.addEventListener('resize', () =>
+		{
+			this.position(this.slideIndex);
+			this.width = this.sliderWrapper.offsetWidth;
+		});
+	},
+}
